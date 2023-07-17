e5f04a7b1b384ffb1c97f08ec701e5f3a55eabb4
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2023-07-17 12:50:51 +0100

    modified max_len parameter

--- run-falcon.py
+++ run-falcon.py
@@ -18,7 +18,9 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-def run_prompt(input:str, output_file:TextIO, max_len:int=len(input)*1.05, num_ret_seq:int=1, ret_full_text:bool=False, ):
+def run_prompt(input:str, output_file:TextIO, max_len:int=-1, num_ret_seq:int=1, ret_full_text:bool=False, ):
+    if max_len == -1: # default to 5% more than input length
+        max_len = len(input)*1.05
     sequences = pipeline(
             input,
             max_length=max_len,
