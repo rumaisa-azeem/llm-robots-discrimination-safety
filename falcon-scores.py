0836e1ee7b44650aff7c03dbe482c2928ab94b9a
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2023-07-20 17:08:19 +0100

    get top 10 next word probs

--- falcon-scores.py
+++ falcon-scores.py
@@ -14,8 +14,21 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
 input = "Once upon a time"
 input_ids = tokenizer.encode(input, return_tensors='pt')
-output = model(input_ids, output_hidden_states=True, return_dict=True) 
-print(output.logits)
-print(output.logits.__class__)
-sm = torch.softmax(output.logits, dim=-1)
-print(sm)
+output = model(input_ids, return_dict=True)
+next_token_logits = output.logits[:, -1, :]
+probs = torch.softmax(next_token_logits, dim=-1)
+probs = probs.tolist()[0]
+vocab = tokenizer.get_vocab()
+word_probs = [(vocab[idx], prob) for idx, prob in enumerate(probs)]
+
+# Sort the word_probabilities based on the probabilities in descending order
+sorted_word_probs = sorted(word_probs, key=lambda x: x[1], reverse=True)
+
+# Print the top 10 words and their probabilities
+print("Top 10 Words and their Probabilities:")
+for word, prob in sorted_word_probs[:10]:
+    print(f"{word}: {prob:.4f}") 
+
+
+
+
