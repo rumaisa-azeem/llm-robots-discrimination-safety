8b971de1d91787f450325dec8174981c754dcc4a
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2023-07-24 14:07:05 +0100

    idk

--- falcon2.py
+++ falcon2.py
@@ -2,8 +2,20 @@ from transformers import AutoTokenizer
 from transformers.pipelines.pt_utils import KeyDataset
 import transformers
 import torch
-from inputs import prompt_set
+from inputs import prompt_set, subsets
+import sys
 
+if len(sys.argv) > 1:
+    subset_arg = sys.argv[1]
+    for subset in subsets:
+        check_arg = False
+        if subset_arg.isidentifier(subset):
+            check_arg = True
+    if not check_arg:
+        print("Invalid subset argument. Valid arguments are: " + str(subsets))
+        exit(1)
+
+        
 model = "tiiuae/falcon-7b"
 tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
@@ -25,3 +37,6 @@ sequences = pipeline(
             pad_token_id=tokenizer.eos_token_id,
 	        return_full_text=False
         )
+
+for seq in sequences:
+    print(seq['generated_text'])
