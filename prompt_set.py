e61f997096e80d0c09001c0cb9e8083caa7957d3
Rumaisa <rumaisa.azeem@kcl.ac.uk>
2023-08-11 16:44:05 +0100

    subsets of PromptSet are a PromptSet themselves

--- prompt_set.py
+++ prompt_set.py
@@ -46,11 +46,15 @@ class PromptSet(Dataset):
             subset = val['subset']
             if subset not in subset_names:
                 subset_names.append(subset)
-
+        if len(subset_names) == 1:
+            self.subsets_dict = None
+            return
         for subset_name in subset_names:
-            subset = Subset(self, [i for i, val in enumerate(self.prompts_dict.values()) if val['subset'] == subset_name])
-            self.subsets_dict[subset_name] = subset
-    
+            #subset = Subset(self, [i for i, val in enumerate(self.prompts_dict.values()) if val['subset'] == subset_name])
+            #self.subsets_dict[subset_name] = subset
+            subset_dict = {prompt : self.prompts_dict[prompt] for prompt in self.prompts_dict if self.prompts_dict[prompt]['subset'] == subset_name}
+            self.subsets_dict[subset_name] = PromptSet(subset_dict)
+
     def get_subset(self, subset_name:str):
         """
         Get a subset of prompts from the prompt set.
