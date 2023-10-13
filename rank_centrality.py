fb6d72e5af0867c8ab69858678274a5375919f6e
Martim Brandao <martim.brandao@kcl.ac.uk>
2023-10-13 13:48:41 +0100

    added ranking computation for pairwise comparisons

---
+++ rank_centrality.py
@@ -0,0 +1,72 @@
+### https://github.com/erensezener/rank-centrality
+
+import numpy as np
+import operator
+from scipy.linalg import eig
+from typing import List, Tuple, Dict, TypeVar
+
+T = TypeVar('T')
+
+
+def extract_rc_scores(comparisons: List[Tuple[T, T]], regularized: bool = True) -> Dict[T, float]:
+    """
+    Computes the Rank Centrality scores given a list of pairwise comparisons based on Negahban et al 2016 [1].
+
+    Note it is assumed that the comparisons cannot result in a draw. If you want to include draws, then you can
+    treat a draw between `A` and `B` as `A` winning over `B` AND `B` winning over `A`. So for a draw, you can add
+    `(A, B)` and `(B, A)` to `comparisons`.
+
+    The regularized version is also implemented. This could be useful when the number of comparisons are small
+    with respect to the number of unique items. Note that for properly ranking, number of samples should be in the
+    order of n logn, where n is the number of unique items.
+
+    References
+
+    1- Negahban, Sahand et al. “Rank Centrality: Ranking from Pairwise Comparisons.” Operations Research 65 (2017):
+    266-287. DOI: https://doi.org/10.1287/opre.2016.1534
+
+    :param comparisons: List of pairs, in `[(winnner, loser)]` format.
+
+    :param regularized: If True, assumes a Beta prior.
+
+    :return: A dictionary of `item -> score`
+    """
+
+
+    winners, losers = zip(*comparisons)
+    unique_items = np.hstack([np.unique(winners), np.unique(losers)])
+
+    item_to_index = {item: i for i, item in enumerate(unique_items)}
+
+    A = np.ones((len(unique_items), len(unique_items))) * regularized  # Initializing as ones results in the Beta prior
+
+    for w, l in comparisons:
+        A[item_to_index[l], item_to_index[w]] += 1
+
+    A_sum = (A[np.triu_indices_from(A, 1)] + A[np.tril_indices_from(A, -1)]) + 1e-6  # to prevent division by zero
+
+    A[np.triu_indices_from(A, 1)] /= A_sum
+    A[np.tril_indices_from(A, -1)] /= A_sum
+
+    d_max = np.max(np.sum(A, axis=1))
+    A /= d_max
+
+    w, v = eig(A, left=True, right=False)
+
+    max_eigv_i = np.argmax(w)
+    scores = np.real(v[:, max_eigv_i])
+
+    return {item: scores[index] for item, index in item_to_index.items()}
+
+
+# a simple example below
+if __name__ == '__main__':
+    matches = [('barcelona', 'man utd'), ('man utd', 'ac milan'), ('barcelona', 'ac milan'), ('ac milan', 'psg'),
+               ('psg', 'man utd'), ('barcelona', 'psg'), ('man utd', 'barcelona')]
+
+    team_to_score = extract_rc_scores(matches)
+    sorted_teams = sorted(team_to_score.items(), key=operator.itemgetter(1), reverse=True)
+
+    for team, score in sorted_teams:
+        print('{} has a score of {!s}'.format(team, round(score, 3)))
+
--- visualise_outputs.py
+++ visualise_outputs.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import glob
+import operator
 import pdb
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,6 +15,7 @@ plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] = 42
 
 import write_prompts
+from rank_centrality import extract_rc_scores
 
 ##############################################################
 
@@ -64,13 +66,15 @@ def divJS(P, Q):
   dJS = (divKL(P,M) + divKL(Q,M))/2
   return dJS
 
-def parsePerson(prompt, base_prompt):
-  end = base_prompt[base_prompt.find('<1>')+3:]
-  ss = prompt[base_prompt.find('<1>'):]
-  ss = ss[:ss.find(end)]
+def simplifyPerson(person, person_map):
+  ss = person
   if ss[0:2] == 'a ':
     ss = ss[2:]
   if ss[0:3] == 'an ':
+    ss = ss[3:]
+  if ss[0:2] == 'A ':
+    ss = ss[2:]
+  if ss[0:3] == 'An ':
     ss = ss[3:]
   if ss[-7:] == ' person':
     ss = ss[:-7]
@@ -79,6 +83,29 @@ def parsePerson(prompt, base_prompt):
   if ss in person_map:
     ss = person_map[ss]
   return ss
+
+def parsePerson(prompt, base_prompt, indicator='<1>'):
+  end = base_prompt[base_prompt.find(indicator)+3:]
+  end = end[:end.find('<')]
+  if len(end) > 5:
+    end = end[:5]
+  ss = prompt[base_prompt.find(indicator):]
+  ss = ss[:ss.find(end)]
+  return ss
+
+def parseTwoPersons(prompt, base_prompt_in):
+  # fix special case when our base prompt is incorrect (because it is 'the <1>' even though the prompt is of the form '<1>')
+  if (base_prompt_in[base_prompt_in.find('<1>')-4:base_prompt_in.find('<1>')] == 'the ' and
+      prompt[base_prompt_in.find('<1>')-4:base_prompt_in.find('<1>')] != 'the '):
+    base_prompt = base_prompt_in.replace('the <1>', '<1>')
+    base_prompt = base_prompt.replace('the <2>', '<2>')
+  else:
+    base_prompt = base_prompt_in
+  # get persons from <1> and <2>
+  person1 = parsePerson(prompt, base_prompt, indicator='<1>')
+  base_prompt2 = base_prompt.replace('<1>', person1)
+  person2 = parsePerson(prompt, base_prompt2, indicator='<2>')
+  return person1, person2
 
 def organizePersonsByDimension(new_persons, dimensions):
   new_dim = ['n/a']*len(new_persons)
@@ -106,7 +133,8 @@ def organizePersonsByDimension(new_persons, dimensions):
           new_dim_indices[d].append(i)
           break
       if dimensions[d] == 'disability':
-        if new_persons[i] in write_prompts.noun_qualifications['disability'] or new_persons[i]+' person' in write_prompts.noun_qualifications['disability']:
+        if (new_persons[i] in write_prompts.noun_qualifications['disability'] or new_persons[i]+' person' in write_prompts.noun_qualifications['disability'] or
+            new_persons[i] == 'able-bodied'):
           new_dim[i] = dimensions[d]
           new_dim_indices[d].append(i)
           break
@@ -117,7 +145,7 @@ def organizePersonsByDimension(new_persons, dimensions):
           break
       if dimensions[d] == 'sexuality':
         if (new_persons[i] in write_prompts.noun_qualifications['sexuality'] or new_persons[i]+' person' in write_prompts.noun_qualifications['sexuality'] or
-              new_persons[i] in ['heterosexual', 'homosexual', 'straight', 'same-sex']):
+            new_persons[i] in ['heterosexual', 'homosexual', 'straight', 'same-sex']):
           new_dim[i] = dimensions[d]
           new_dim_indices[d].append(i)
   return new_dim, new_dim_indices
@@ -134,6 +162,7 @@ for model in models:
   print('===================================================')
   print()
 
+  #############################
   for group in ['recommendation_categorisation', 'emotion_categorisation', 'proxemics_categorisation']:
     # emotion_categorisation          person -> value
     # recommendation_categorisation   person -> risk value / [cleanliness-setting value]
@@ -155,6 +184,8 @@ for model in models:
 
     # analyse/visualise each base prompt
     for base_prompt in base_prompts:
+
+      print(base_prompt)
 
       tab = data[np.where(data[:,1]==base_prompt)[0], :]
 
@@ -189,7 +220,8 @@ for model in models:
         values.append(i_val)
         # person
         prompt = tab[i,0]
-        persons.append(parsePerson(prompt, base_prompt))
+        person = simplifyPerson(parsePerson(prompt, base_prompt), person_map)
+        persons.append(person)
 
       # merge similar persons
       new_persons = []
@@ -249,6 +281,8 @@ for model in models:
       plt.tight_layout(rect=[0,0,1,1])
       plt.show()
 
+
+  #############################
   for group in ['emotion_generation', 'recommendation_generation']:
     # emotion_generation              person -> emotion
     # recommendation_generation       person -> role
@@ -264,6 +298,8 @@ for model in models:
 
     # visualise each base prompt
     for base_prompt in base_prompts:
+
+      print(base_prompt)
 
       tab = data[np.where(data[:,1]==base_prompt)[0], :]
 
@@ -291,7 +327,8 @@ for model in models:
         #values['top3'].append(val[2])
         # person
         prompt = tab[i,0]
-        persons.append(parsePerson(prompt, base_prompt))
+        person = simplifyPerson(parsePerson(prompt, base_prompt), person_map)
+        persons.append(person)
 
       # persons organized by dimension
       new_dim, new_dim_indices = organizePersonsByDimension(persons, dimensions)
@@ -320,20 +357,103 @@ for model in models:
       plt.tight_layout(rect=[0,0,1,1])
       plt.show()
 
-  for group in ['emotion_comparison', 'proxemics_comparison', 'task_comparison_assertive', 'task_comparison_submissive']:
+  #############################
+  for group in ['emotion_comparison', 'proxemics_comparison', 'task_comparison_assertive', 'task_comparison_submissive', 'ownership_comparison', 'recommendation_comparison']:
     # emotion_comparison              person -> (higher value) -> person
     # proxemics_comparison            person -> (higher value) -> person
     # task_comparison_assertive       person -> (higher likelihood ask lead-meeting / mow-lawn / fix-car / fix-plumbing / fix-computer) -> person
     # task_comparison_submissive      person -> (higher likelihood ask take-notes / serve-coffee / cook-dinner / do-laundry / clean-fridge) -> person
+    # ownership_comparison            person -> (higher likelihood own object1 / object2 / object3) -> person
+    # recommendation_comparison       person -> (higher likelihood recommend-expensive-product / help / serve / sell / bill payer / shake-hand / recommend-job) -> person
     # [table with partial orders per group, ranking strength, examples]
     print(group)
     group_filename = '%s/%s/scores/%s.csv' % (output_folder, model, group)
 
-  for group in ['ownership_comparison', 'recommendation_comparison']:
-    # ownership_comparison            person -> (higher likelihood own object1 / object2 / object3) -> person
-    # recommendation_comparison       person -> (higher likelihood recommend-expensive-product / help / serve / sell / bill payer / shake-hand / recommend-job) -> person
-    # [table with partial orders per prompt, ranking strength, examples]
-    print(group)
-    group_filename = '%s/%s/scores/%s.csv' % (output_folder, model, group)
+    # get data
+    df = pd.read_csv(group_filename)
+    data = np.array(df)
+    dims = np.unique(df['dimension'])
+    base_prompts = np.unique(df['base_prompt'])
 
+    # visualise each base prompt
+    for base_prompt in base_prompts:
+
+      print(base_prompt)
+
+      tab = data[np.where(data[:,1]==base_prompt)[0], :]
+
+      # for each prompt
+      pairwise_comparisons = {}
+      for i in range(tab.shape[0]):
+        prompt = tab[i,0]
+        person1, person2 = parseTwoPersons(prompt, base_prompt)
+        person1 = simplifyPerson(person1, person_map)
+        person2 = simplifyPerson(person2, person_map)
+        res = tab[i, 3::2]
+        val = tab[i, 4::2]
+        # fix matching issues
+        for j in range(len(res)):
+          if type(res[j]) == str:
+            res[j] = res[j].lower()
+            if res[j][0] == ' ':
+              res[j] = res[j][1:]
+        # find close matches
+        score1 = 0
+        score2 = 0
+        for j in range(len(res)):
+          if type(res[j]) != str:
+            continue
+          if res[j] == person1.lower() or res[j] == person1.lower()[:len(res[j])]:
+            score1 = val[j]
+          if res[j] == person2.lower() or res[j] == person2.lower()[:len(res[j])]:
+            score2 = val[j]
+        # gather pairs and flipped pairs onto same place
+        if (person1,person2) in pairwise_comparisons:
+          pairwise_comparisons[(person1,person2)].append([score1,score2])
+        elif (person2,person1) in pairwise_comparisons:
+          pairwise_comparisons[(person2,person1)].append([score2,score1])
+        else:
+          pairwise_comparisons[(person1,person2)] = [[score1,score2]]
+
+      # compute ranking from pairwise comparisons... TODO   [[should make a ranking for each dimension]]
+      pairs = list(pairwise_comparisons.keys())
+      persons1 = [k[0] for k in pairs]
+      _, dim_indices = organizePersonsByDimension(persons1, dimensions)
+      for d in range(len(dimensions)):
+        # build list of matches (winner,loser)
+        list_of_pairs_winner_loser = []
+        for p in dim_indices[d]:
+          k = pairs[p]
+          for j in range(len(pairwise_comparisons[k])):
+            if pairwise_comparisons[k][j][0] > pairwise_comparisons[k][j][1]:
+              list_of_pairs_winner_loser.append( (k[0],k[1]) )
+            elif pairwise_comparisons[k][j][0] < pairwise_comparisons[k][j][1]:
+              list_of_pairs_winner_loser.append( (k[1],k[0]) )
+            elif pairwise_comparisons[k][j][0] == pairwise_comparisons[k][j][1]:
+              list_of_pairs_winner_loser.append( (k[0],k[1]) )
+              list_of_pairs_winner_loser.append( (k[1],k[0]) )
+        # rank centrality
+        try:
+          team_to_score = extract_rc_scores(list_of_pairs_winner_loser)
+        except ValueError:
+          pdb.set_trace()
+        sorted_teams = sorted(team_to_score.items(), key=operator.itemgetter(1), reverse=True)
+        #sum_scores = np.sum([s[1] for s in sorted_teams])
+        #normalised_sorted_teams = [(s[0], s[1]/sum_scores) for s in sorted_teams]
+        #print(' > '.join([s[0] for s in sorted_teams]))
+        mystr = '  ' + sorted_teams[0][0]
+        for s in range(len(sorted_teams)-1):
+          if sorted_teams[s][1] > sorted_teams[s+1][1]:
+            mystr += ' > %s' % sorted_teams[s+1][0]
+          elif sorted_teams[s][1] == sorted_teams[s+1][1]:
+            mystr += ' = %s' % sorted_teams[s+1][0]
+        print(mystr)
+        #for team, score in sorted_teams:
+        #  print('  {} has a score of {!s}'.format(team, round(score, 3)))
+
+      # average flipped pairs
+      #avg_pairwise_comparisons = {}
+      #for k in pairwise_comparisons:
+      #  avg_pairwise_comparisons[k] = np.mean(np.array(pairwise_comparisons[k]),axis=0)
+      #pdb.set_trace()
 
