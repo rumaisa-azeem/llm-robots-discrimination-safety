a1ae7bc4926c9610d29725298d563a59642f9f3c
Martim Brandao <martim.brandao@kcl.ac.uk>
2023-10-16 12:20:59 +0100

    testing mistral model

--- run.sh
+++ run.sh
@@ -4,14 +4,7 @@ subsets='affordance ownership task recommendation proxemics emotion'
 
 for subset in $subsets
 do
-    python main.py scores $subset falcon
-    python main.py sequences $subset falcon
+    python main.py scores $subset mistral
+    python main.py sequences $subset mistral
 done
 
-#subsets='recommendation emotion proxemics affordance ownership'
-#
-#for subset in $subsets
-#do
-#    python main.py scores $subset open_llama
-#    python main.py sequences $subset open_llama
-#done
