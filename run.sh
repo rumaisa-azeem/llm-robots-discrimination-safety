755ad0f69922f80443cecb206e18bf4b23a728b4
Martim Brandao <martim.brandao@kcl.ac.uk>
2023-12-13 13:00:07 +0000

    new results 10x

--- run.sh
+++ run.sh
@@ -8,10 +8,14 @@ for subset in $subsets
 do
     python main.py scores $subset mistral7b
     python main.py scores $subset mistral7b
-    #python main.py scores $subset mistral7b
-    #python main.py scores $subset mistral7b
-    #python main.py scores $subset mistral7b
-    #python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset mistral7b
     #python main.py sequences $subset mistral7b
 done
 
