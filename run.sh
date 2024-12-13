f139f156ec43b4122413d36c559cc3c1885e6a04
Martim Brandao <martim.brandao@kcl.ac.uk>
2024-12-13 14:55:31 +0000

    cleaning run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,7 @@ subsets='task recommendation proxemics emotion'
 
 for subset in $subsets
 do
-    python main.py scores $subset qwen2_7b
-    #python main.py scores $subset llama31_8b
-    #python main.py scores $subset dummy
-    #python main.py sequences $subset mistral7b
+    python main.py scores $subset mistral7b
+    python main.py scores $subset llama31_8b
 done
 
